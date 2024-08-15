25c243dc1d3b8243cab1e9f380fbe1f8f3ff4f69
Mohammed Muzzammil Shareef <mohdmuzzammil633@gmail.com>
2024-08-15 22:52:34 +0530

    pagination

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,20 +1,21 @@
-const PropertyModel = require("../models/propertyModel");
 
-class PropertyController {
-  static getAllProperties(req, res) {
-    const properties = PropertyModel.getAllProperties();
-    res.json(properties);
+const data = require("../data/data.json"); 
+
+exports.getProperties = (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 15;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  if (page < 1 || limit < 1) {
+    return res.status(400).json({ message: "Invalid page or limit" });
   }
 
-  static getPropertiesByCity(req, res) {
-    const city = req.params.city;
-    const properties = PropertyModel.getPropertyByCity(city);
-    if (properties.length > 0) {
-      res.json(properties);
-    } else {
-      res.status(404).json({ message: "No properties found in this city" });
-    }
+  if (startIndex >= data.length) {
+    return res.status(404).json({ message: "No more data available" });
   }
-}
 
-module.exports = PropertyController;
+  const result = data.slice(startIndex, endIndex);
+
+  res.json(result);
+};
--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -3,7 +3,6 @@ const PropertyController = require("../controllers/propertyController");
 
 const router = express.Router();
 
-router.get("/properties", PropertyController.getAllProperties);
-router.get("/properties/:city", PropertyController.getPropertiesByCity);
+router.get("/properties", PropertyController.getProperties);
 
 module.exports = router;
